f3a3b6ba85af2759d4da8bec5f0b335fca5e8224
msashofwan123 <msashofwan123@gmail.com>
2023-04-10 08:01:30 +0700

    Bismillah

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+  import React, { useState } from 'react';
+  import Header from './components/Header';
+  import Footer from './components/Footer';
+  import Transaction from './components/Transaction';
+  import SaldoBox from './components/SaldoBox';
+  import AddTransaction from './components/AddTransaction';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const iniTransactions = [
+    {
+      "id": "987654456789",
+      "tanggal": new Date("01 Nov 2021 9:30").getTime(),
+      "keterangan": "gaji bulanan",
+      "nominal": 250000,
+    },  
+    {
+      "id": "434565432",
+      "tanggal": new Date("03 Nov 2021 19:30").getTime(),
+      "keterangan": "thr",
+      "nominal": 250000,
+    },
+    {
+      "id": "92343289",
+      "tanggal": new Date("20 Nov 2021 11:30").getTime(),
+      "keterangan": "Jajan",
+      "nominal": -120000,
+    },
+  ];
 
-export default App;
+  const App = () => {
+    const [transactions, setTransactions] = useState(iniTransactions);
+
+    const handleTambahTransaction = (data) => {
+      let newTransaction = [
+        ...transactions, data
+      ];
+
+      newTransaction.sort((a,b) => a.tanggal - b.tanggal);
+      setTransactions([...newTransaction]);
+      // console.log('Transaksi'+data)
+    }
+
+    const handleHapusTransaction = (e) => {
+      const result = transactions.findIndex(
+        transaction => (transaction.id === e.target.id)
+      );
+      
+      const newTransactions = transactions;
+      newTransactions.splice(result,1);
+      setTransactions([...newTransactions]);
+    }
+
+    return (
+      <React.Fragment>
+        <Header />
+        <SaldoBox transactions={transactions}/>
+        <Transaction transactions={transactions} onHapusTransaction={handleHapusTransaction}/>
+        <AddTransaction onTambahTransaction={handleTambahTransaction}/>
+        <Footer />
+      </React.Fragment>
+    )
+  }
+
+  export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
